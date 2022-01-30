bd0ce1e045cca843daaedf58eda6aae7a2b48761
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-01-29 23:26:58 -0500

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "John Smith";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  window.bestCustomer = "not bob";
+}
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "Jane Smith";
+}
